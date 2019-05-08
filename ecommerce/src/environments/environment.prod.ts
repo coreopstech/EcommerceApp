5dfc42ee3ae57069703b105d14ab1859a3472664
coreopstech <46955578+coreopstech@users.noreply.github.com>
2019-05-08 19:17:18 +0530

    check in by ajeet

--- ecommerce/src/environments/environment.prod.ts
+++ ecommerce/src/environments/environment.prod.ts
@@ -3,4 +3,7 @@ export const environment = {
   baseUrl:"http://localhost/Ecommerce.WebApi/",
   
   baseImageUrl:"http://localhost/Ecommerce.Web/Upload/ProductImages/",
+  // baseUrl:"http://150.107.189.25/PublishEcommerceWebAPI/",
+  
+  // baseImageUrl:"http://150.107.189.25/PublishEcommerceWeb/Upload/ProductImages/",
 };
--- ecommerce/src/environments/environment.ts
+++ ecommerce/src/environments/environment.ts
@@ -6,6 +6,9 @@ export const environment = {
   production: false,
   baseUrl:"http://localhost/Ecommerce.WebApi/",
   baseImageUrl:"http://localhost/Ecommerce.Web/Upload/ProductImages/",
+  // baseUrl:"http://150.107.189.25/PublishEcommerceWebAPI/",
+  
+  // baseImageUrl:"http://150.107.189.25/PublishEcommerceWeb/Upload/ProductImages/",
 };
 
 /*
